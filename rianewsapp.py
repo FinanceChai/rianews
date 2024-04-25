13bdfc46d482ba4b54ed0f002434e7880e5926c7
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-04-25 17:19:01 +0100

    Update rianewsapp.py

--- rianewsapp.py
+++ rianewsapp.py
@@ -2,11 +2,8 @@
 from dotenv import load_dotenv
 import os
 
-# Specify the path to the .env file
-dotenv_path = "/home/rianews/rianewsapp.env"
 
-# Load environment variables from the .env file
-load_dotenv(dotenv_path)
+load_dotenv()
 
 # Print the value of TELEGRAM_TOKEN environment variable
 print("TELEGRAM_TOKEN:", os.getenv("TELEGRAM_TOKEN"))
